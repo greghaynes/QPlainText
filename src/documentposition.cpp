2d5ff75c44ba98af33833d4ca955319ed49f449f
Gregory Haynes <greg@greghaynes.net>
2009-07-24 17:02:44 -0700

    Initial commit

---
+++ src/documentposition.cpp
@@ -0,0 +1,11 @@
+/*
+ *  documentposition.cpp
+ *  qsourceview
+ *
+ *  Created by Gregory Haynes on 7/24/09.
+ *  Copyright 2009 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "documentposition.h"
+
---
+++ src/documentrange.cpp
@@ -0,0 +1,11 @@
+/*
+ *  documentrange.cpp
+ *  qsourceview
+ *
+ *  Created by Gregory Haynes on 7/24/09.
+ *  Copyright 2009 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "documentrange.h"
+
