3b60b3d1f65047e939b4a29771de90533e8e3b52
Gregory Haynes <greg@greghaynes.net>
2011-04-26 15:25:03 -0700

    Added releases/devel to mainpage..h

--- src/mainpage.h
+++ src/mainpage.h
@@ -6,5 +6,11 @@
  * use the library while allowing editors to customize the library's appearence
  * and functionality as they see fit.
  *
+ * @section Releases
+ * There are currently no releases for this project.
+ *
+ * @section Development
+ * The development site can be found at https://github.com/greghaynes/QPlainText
+ *
  */
 
