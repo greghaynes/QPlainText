711fd794f2478d422a718d3e35609856f236f87d
Gregory Haynes <greg@greghaynes.net>
2011-04-26 15:06:18 -0700

    Added mainpage.h with doxygen mainpage

---
+++ src/mainpage.h
@@ -0,0 +1,10 @@
+/**
+ * @mainpage QPlainText
+ *
+ * QPlaintext is a plain text editor library which depends only on Qt.
+ * It is meant to allow a consistent API for plugins across editors that
+ * use the library while allowing editors to customize the library's appearence
+ * and functionality as they see fit.
+ *
+ */
+
